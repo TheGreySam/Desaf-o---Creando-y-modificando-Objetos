763718178bf78b10dcda9bbba8f4e9d870534c33
Sebastián Gómez Letelier <sgl1994@gmail.com>
2021-07-23 18:25:04 -0400

    second commit

--- index.js
+++ index.js
@@ -4,16 +4,37 @@ function Consultorio(nombre, pacientes) {
 }
 
 Consultorio.prototype.todosLosPacientes = function () {
-  console.log(Paciente)
-}
+  console.log(JSON.parse(JSON.stringify(this.pacientes)));
+};
+
+Consultorio.prototype.buscarPaciente = function (nombrePacienteABuscar) {
+  var indice = -1;
+
+  for (var i = 0; i < this.pacientes.length; i++) {
+    if (this.pacientes[i].nombre == nombrePacienteABuscar) {
+      indice = i;
+    }
+  }
+
+  if (indice >= 0) {
+    console.log(
+      "Paciente encontrado, sus datos son: ",
+      JSON.parse(JSON.stringify(this.pacientes[indice]))
+    );
+  } else {
+    console.log("Paciente no encontrado, intenta con otro nombre");
+  }
+};
 
 function Paciente(nombre, edad, rut, diagnostico) {
+  
   var _nombre = nombre;
   var _edad = edad;
   var _rut = rut;
   var _diagnostico = diagnostico;
 
   Object.defineProperty(this, "nombre", {
+    enumerable: true,
     get: function () {
       return _nombre;
     },
@@ -23,24 +44,27 @@ function Paciente(nombre, edad, rut, diagnostico) {
   });
 
   Object.defineProperty(this, "edad", {
+    enumerable: true,
     get: function () {
       return _edad;
     },
-    set: function (nuevaEdad) {
-      _edad = nuevaEdad;
+    set: function (nuevoEdad) {
+      _edad = nuevoEdad;
     },
   });
 
   Object.defineProperty(this, "rut", {
+    enumerable: true,
     get: function () {
       return _rut;
     },
     set: function (nuevoRut) {
-      _rut = nuevRut;
+      _rut = nuevoRut;
     },
   });
 
   Object.defineProperty(this, "diagnostico", {
+    enumerable: true,
     get: function () {
       return _diagnostico;
     },
@@ -48,8 +72,33 @@ function Paciente(nombre, edad, rut, diagnostico) {
       _diagnostico = nuevoDiagnostico;
     },
   });
-
 }
 
-const PacienteRobertoHurtado = new Paciente("Roberto", 25, "1.000.555-5", "Demasiada sensualidad");
-console.log(PacienteRobertoHurtado.nombre);
+///////////////////////
+const PacienteRobertoHurtado = new Paciente(
+  "Roberto",
+  30,
+  "1.222.333-4",
+  "Demasiada sensualidad" // 1313
+);
+
+const PacienteCarolinaSanchez = new Paciente(
+  "Carolina",
+  30,
+  "1.222.333-4",
+  "Resfrio"
+);
+
+const ConsultorioJuanBautista = new Consultorio("Juan Bautista", [
+  PacienteCarolinaSanchez,
+  PacienteRobertoHurtado,
+]);
+
+///////////////////////////////
+
+ConsultorioJuanBautista.todosLosPacientes();
+ConsultorioJuanBautista.buscarPaciente("Roberto");
+ConsultorioJuanBautista.buscarPaciente("Carolina");
+ConsultorioJuanBautista.buscarPaciente("Angela");
+
+console.log(PacienteCarolinaSanchez[0]);
